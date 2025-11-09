bc0a2a6da7c9bee50dd89ed923709bf63d0e25b6
JacNal <jacob@nalholm.dk>
2025-11-09 17:13:47 +0100

    Updated the ALU to also include the logic for updating NZP also implemented first iteration of the registers. I just chose 16 registers arbitrarily. Registers have not been tested and nzp has not been tested either.

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -7,6 +7,7 @@ class ALU extends Module {
     val operand2 = Input(UInt(32.W))
     val sel = Input(UInt(2.W))
     val result = Output(UInt(32.W))
+    val nzp = Output(UInt(3.W))
   })
 
   object ALUOp {
@@ -15,14 +16,28 @@ class ALU extends Module {
     val and = "b10".U(2.W)
   }
 
-  io.result := 0.U
+
+  val ALUResult = Wire(0.U(32.W))
+
   switch(io.sel) {
-    is(ALUOp.add) { io.result := io.operand1 + io.operand2}
-    is(ALUOp.sub) { io.result := io.operand1 - io.operand2}
-    is(ALUOp.and) { io.result := io.operand1 & io.operand2}
+    is(ALUOp.add) { ALUResult := io.operand1 + io.operand2}
+    is(ALUOp.sub) { ALUResult := io.operand1 - io.operand2}
+    is(ALUOp.and) { ALUResult := io.operand1 & io.operand2}
   }
 
+  io.result := ALUResult
 
+  val nzpReg = RegInit(0.U(3.W))
+
+  val isZero = ALUResult === 0.U
+  val isNeg  = ALUResult(31)
+  val isPos  = !isNeg & !isZero
+
+  when(io.sel === ALUOp.add || io.sel === ALUOp.sub) {
+    nzpReg := Cat(isNeg, isZero, isPos)
+  }
+
+  io.nzp := nzpReg
 
   //Implement this module here
 
--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -1,11 +1,25 @@
 import chisel3._
-import chisel3.util._
 
 class RegisterFile extends Module {
   val io = IO(new Bundle {
+    val aSel = Input(UInt(4.W))
+    val bSel = Input(UInt(4.W))
+    val dataToWrite = Input(UInt(32.W))
+    val writeSel = Input(UInt(4.W))
+    val writeEnable = Input(Bool())
+    val outA = Output(UInt(32.W))
+    val outB = Output(UInt(32.W))
     //Define the module interface here (inputs/outputs)
   })
 
+  val registers = Reg(Vec(16, UInt(32.W)))
+
+  io.outA := registers(io.aSel)
+  io.outB := registers(io.bSel)
+
+  when (io.writeEnable) {
+    registers(io.writeSel) := io.dataToWrite
+  }
   //Implement this module here
 
 }
